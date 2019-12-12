f872dc791f660e6df3798cecbbbbe98ce780edbc
Leandro Rodrigues <leandropcsx@gmail.com>
2019-12-11 21:38:43 -0300

    minor refactoring

--- src/components/QueueList/index.js
+++ src/components/QueueList/index.js
@@ -19,35 +19,38 @@ import {
 import {PERSON_QUEUE} from '~/data/dummy-data';
 
 const QueueList = () => {
-  const offset = 67;
   let positions = 0;
+  const offset = 67;
+  const margin = 17;
 
   const translateY = new Animated.Value(0);
 
-  const [actualPosition, setActualPosition] = useState(PERSON_QUEUE.find(p => p.selected).position);
+  const personQueue = PERSON_QUEUE.find(p => p.selected);
+
+  const [actualPosition, setActualPosition] = useState(personQueue.position);
   const [positionValue] = useState(offset);
 
-  const [positionSelectedHighlight, setPositionSelectedHighlight] = useState(17 + 67 * (PERSON_QUEUE.find(p => p.selected).position - 1));
+  const [positionSelectedHighlight, setPositionSelectedHighlight] = useState(
+    margin + offset * (personQueue.position - 1),
+  );
 
   useEffect(() => {
     setTimeout(() => {
       checkQueue();
-    // }, Math.floor(Math.random() * 10) * 1000);
-    }, 1000);
+    }, Math.floor(Math.random() * 10) * 1000);
   }, [actualPosition]);
 
   const checkQueue = () => {
-    if (PERSON_QUEUE.find(p => p.selected).position > 1) {
+    if (personQueue.position > 1) {
       Animated.timing(translateY, {
         toValue: -positionValue,
         duration: 350,
         useNativeDriver: true,
       }).start(() => {
-        PERSON_QUEUE.find(p => p.selected).position =
-          PERSON_QUEUE.find(p => p.selected).position - 1;
-        const initial = 17 + 67 * (PERSON_QUEUE.find(p => p.selected).position - 1);
+        personQueue.position = personQueue.position - 1;
+        const initial = margin + offset * (personQueue.position - 1);
         setPositionSelectedHighlight(initial);
-        setActualPosition(PERSON_QUEUE.find(p => p.selected).position);
+        setActualPosition(personQueue.position);
       });
     }
   };
@@ -74,9 +77,8 @@ const QueueList = () => {
         data={PERSON_QUEUE}
         keyExtractor={item => String(item.id)}
         scrollEnabled={false}
-        renderItem={({item}) => {
+        renderItem={() => {
           positions++;
-
           return (
             <QueueItem>
               <QueueNumberContainer>
